62bbb8b3e1ab6be79c59ad18c34013d2fab53884
Maghav <55014387+Maghav@users.noreply.github.com>
2021-09-14 11:49:00 +0530

    Create bubble_sort.py

---
+++ bubble_sort.py
@@ -0,0 +1,30 @@
+# In first iteration, Starting from the first index, compare the first and the second elements. If the first element is greater than the second element, they are swapped
+# Now, compare the second and the third elements. Swap them if they are not in order
+# The above process goes on until the last element and the same process goes on for the remaining iterations
+# After each iteration, the largest element among the unsorted elements is placed at the end.
+
+
+# Bubble sort in Python
+
+def func(a):
+    # loop to access each array element
+    for i in range(len(a)):
+
+        # loop to compare array elements
+        for j in range(0, len(a) - i - 1):
+
+            # compare two adjacent elements
+            # change > to < to sort in descending order
+            if a[j] > a[j + 1]:
+                # swapping elements if elements
+                # are not in the intended order
+                temp = a[j]
+                a[j] = a[j + 1]
+                a[j + 1] = temp
+
+
+b = [5,6,1,2,8,2,0,12,6,7]
+
+func(b)
+
+print(b)
